f404bea1909f960e973d0a543cc69ccb70ac1d71
oleksii.neivanov <oleksii.neivanov@yalantis.net>
2022-10-10 01:10:18 +0300

    test healthcheck

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,5 @@
 var request = require("request"),
     assert = require('assert'),
-    demoApp = require("../src/app.js"),
     base_url = "http://localhost:3000/healthcheck";
 
 describe("Test healthcheck", function() {
@@ -9,7 +8,6 @@ describe("Test healthcheck", function() {
     it("returns status code 200", function(done) {
       request.get(base_url, function(error, response, body) {
         assert.equal(200, response.statusCode);
-        demoApp.close();
         done();
       });
     });
